b444bec8a8d8478b9684c011331f269886bcf96c
simri77 <simretmesfin97@gmail.com>
2022-09-16 09:36:26 -0700

    error

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,9 @@
-# creates a file in /tmp
-
-file { '/tmp/school':
-  content =>'I love Puppet',
+#Create file school
+file { 'school':
+  ensure  => 'present',
+  content => 'I love Puppet',
+  group   => 'www-data',
   mode    => '0744',
   owner   => 'www-data',
-  group   => 'www-data',
-  }
+  path    => '/tmp/holberton',
+}
